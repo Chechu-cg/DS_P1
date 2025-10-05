1dd61a6638341b4728106ef436d34896ce6de595
Chechu-cg <59642681+Chechu-cg@users.noreply.github.com>
2025-10-05 22:36:38 +0200

    Se cambia la clase SubstitutionCipher

--- P1/src/e1/SubstitutionCipher.java
+++ P1/src/e1/SubstitutionCipher.java
@@ -3,51 +3,74 @@ package e1;
 import java.util.Scanner;
 
 public class SubstitutionCipher {
+    private static final String mayusculas = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static final String minusculas = "abcdefghijklmnopqrstuvwxyz";
 
-    // Usar String encode(int key, String text)
-    // Usar String decode(int key, String text)
-
-    // Clases: String, Character, StringBuilder
-
-    private static String mayusculas = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    private static String minusculas = "abcdefghijklmnopqrstuvwxyz";
-    private static int key = 3;
-    private String coded;
-
-    public static String encode(int key, String text) {
-
+    public static String encode(int key, String arg){
+        StringBuilder text = new StringBuilder(arg);
         for (int i = 0; i < text.length(); i++) {
-
             for (int j = 0; j < minusculas.length(); j++) {
                 if (text.charAt(i) == minusculas.charAt(j)) {
                     if (j + key > minusculas.length() - 1) {
-                        System.out.print(minusculas.charAt(j + key - minusculas.length()));
+                        text.setCharAt(i, minusculas.charAt(j + key - minusculas.length()));
                     } else {
-                        System.out.print(minusculas.charAt(j + key));
+                        text.setCharAt(i, minusculas.charAt(j + key));
                     }
+                    break;
                 } else if (text.charAt(i) == mayusculas.charAt(j)) {
                     if (j + key > mayusculas.length() - 1) {
-                        System.out.print(mayusculas.charAt(j + key - mayusculas.length()));
+                        text.setCharAt(i, mayusculas.charAt(j + key - mayusculas.length()));
                     } else {
-                        System.out.print(mayusculas.charAt(j + key));
+                        text.setCharAt(i, mayusculas.charAt(j + key));
                     }
+                    break;
+                }
+            }
+
+        }
+        System.out.println("coded = " + text);
+
+        return text.toString();
+    }
+
+    public static String decode(int key, String arg){
+        StringBuilder text = new StringBuilder(arg);
+
+        for (int i = 0; i < text.length(); i++) {
+            for (int j = 0; j < mayusculas.length(); j++) {
+
+                if (text.charAt(i) == minusculas.charAt(j)) {
+                    if (j-key < 0) {
+                        text.setCharAt(i, minusculas.charAt(minusculas.length() + j - key));
+                    } else {
+                        text.setCharAt(i, minusculas.charAt(j-key));
+                    }
+                    break;
+                } else if (text.charAt(i) == mayusculas.charAt(j)) {
+                    if (j-key < 0) {
+                        text.setCharAt(i, mayusculas.charAt(mayusculas.length() + j - key));
+                    }  else {
+                        text.setCharAt(i, mayusculas.charAt(j-key));
+                    }
+                    break;
                 }
             }
         }
 
-
-        return text;
+        System.out.println("decoded = " + text);
+        return text.toString();
     }
 
-    void main(String[] args) {
+    public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
 
         System.out.println("Enter text:");
         String text = scan.nextLine();
 
-        coded = encode(key, text);
-
+        int key = 3;
+        String coded = encode(key, text);
+        decode(key, coded);
 
     }
 
