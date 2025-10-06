1970c3dc6202639b5cb95c79384a87d00119ceb8
Chechu-cg <59642681+Chechu-cg@users.noreply.github.com>
2025-10-06 11:50:42 +0200

    Se crea el test para la clase Rectangle

---
+++ test/e3/RectangleTest.java
@@ -0,0 +1,60 @@
+package e3;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RectangleTest {
+
+    @Test
+    void getBase() {
+    }
+
+    @Test
+    void getHeight() {
+    }
+
+    @Test
+    void setBase() {
+    }
+
+    @Test
+    void setHeight() {
+    }
+
+    @Test
+    void isSquare() {
+    }
+
+    @Test
+    void area() {
+    }
+
+    @Test
+    void perimeter() {
+    }
+
+    @Test
+    void diagonal() {
+    }
+
+    @Test
+    void turn() {
+    }
+
+    @Test
+    void putHorizontal() {
+    }
+
+    @Test
+    void putVertical() {
+    }
+
+    @Test
+    void testEquals() {
+    }
+
+    @Test
+    void testHashCode() {
+    }
+}
